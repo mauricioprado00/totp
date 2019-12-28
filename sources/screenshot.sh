3cac05b8f3b5e1621702a5839c777fa705025923
Mauricio Prado <mauricioprado00@gmail.com>
2019-12-28 10:32:23 +0200

    Combine all screenshot tools in one function

--- sources/screenshot.sh
+++ sources/screenshot.sh
@@ -4,7 +4,25 @@ function screenshot-take
 {
     local binary_file
     local wait=1
-    local tools=("maim" "import" "gnome-screenshot" "scrot" "spectacle")
+    local tools=(
+        "maim"
+        "import"
+        "gnome-screenshot"
+        "scrot"
+        "spectacle"
+    )
+    local tools_format=(
+        # maim
+        '%s %s'
+        # import
+        '%s %s'
+        # gnome-screenshot
+        '%s -f %s -a'
+        # scrot
+        '%s %s'
+        # spectacle
+        '%s -b -o %s -r'
+    )
 
     >&2 echo Waiting $wait seconds before taking screenshot
     sleep 1
@@ -12,76 +30,24 @@ function screenshot-take
     for (( ctool=0; ctool < ${#tools[@]}; ctool++ )); do        
         binary_file=$(which ${tools[ctool]})
         if [ $? -eq 0 ]; then
-            screenshot-take-${tools[ctool]} $binary_file
+            screenshot-take-generic "$binary_file" "${tools_format[ctool]}"
             return
         fi
     done
 
-    >&2 "Missing tool to take screenshot. Please install either imagemagik, scrot, gnome-screenshot or spectacle"
+    >&2 "Missing tool to take screenshot. Please install either imagemagik, scrot, gnome-screenshot, spectacle or malm"
     return 1
 }
 
-# try use maim
-function screenshot-take-maim
+function screenshot-take-generic
 {
     local file=$(mktemp /tmp/XXXXXXX.png)
+    local cmd=$(printf "$2" "$1" "$file")
     >&2 echo creating screenshot with $1 in $file
-    $1 ${file}
+    $cmd
     if [ $? -ne 0 ]; then
         return 1
     fi
 
     echo ${file}    
-}
-
-# try use spectacle
-function screenshot-take-spectacle
-{
-    local file=$(mktemp /tmp/XXXXXXX.png)
-    >&2 echo creating screenshot with $1 in $file
-    $1 -b -o ${file} -r
-    if [ $? -ne 0 ]; then
-        return 1
-    fi
-
-    echo ${file}    
-}
-
-# try use gnome-screenshot
-function screenshot-take-gnome-screenshot
-{
-    local file=$(mktemp /tmp/XXXXXXX.png)
-    >&2 echo creating screenshot with $1 in $file
-    $1 -f ${file} -a
-    if [ $? -ne 0 ]; then
-        return 1
-    fi
-
-    echo ${file}    
-}
-
-# try use scrot
-function screenshot-take-scrot
-{
-    local file=$(mktemp /tmp/XXXXXXX.png)
-    >&2 echo creating screenshot with $1 in $file
-    $1 ${file}
-    if [ $? -ne 0 ]; then
-        return 1
-    fi
-
-    echo ${file}    
-}
-
-# try use import (imagemagik)
-function screenshot-take-import
-{
-    local file=$(mktemp /tmp/XXXXXXX.png)
-    >&2 echo creating screenshot with $1 in $file
-    $1 ${file}
-    if [ $? -ne 0 ]; then
-        return 1
-    fi
-
-    echo ${file}
 }
