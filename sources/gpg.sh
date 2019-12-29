6d1fe569072c61c8bc088301919603569a04a5c9
Mauricio Prado <mauricioprado00@gmail.com>
2019-12-29 16:16:10 +0200

    Create keys if no key, use key if single secret key

--- sources/gpg.sh
+++ sources/gpg.sh
@@ -1,9 +1,47 @@
+function gpg-check-keys
+{
+    local keys=$(gpg2 --list-secret-keys --keyid-format long)
+    local choice
+    if [ -z "$keys" ]; then
+        echo "You dont have private keys. Do you wish to create one now?"
+        read -p "Your choice (y/n)?" choice
+        case $choice in
+            y|Y|yes) 
+                gpg-generate-key
+            ;;
+            *)
+                echo "Please retry after creating a key with:"
+                echo "gpg --full-gen-key"
+                return 1
+            ;;
+        esac
+    fi
+}
 
+function gpg-generate-key
+{
+    gpg --full-gen-key 2>/dev/null
+}
 function ggp-encrypt-account-key
 {
     local account_name="$1"
     local key_file=$(account-key-file "${account_name}")
     local key_file_encrypted=$(account-key-file-encrypted "${account_name}")
+    local keys
+    local uid
+
+    gpg-check-keys
+
+    if [ $? -ne 0 ]; then
+        echo "Abort missing encrypting key"
+        return 4
+    fi
+
+    # if only one key, use that
+    if [ $(gpg --list-secret-keys --keyid-format LONG |  grep ssb | wc -l) == 1 ]; then
+        uid=$(gpg --list-secret-keys --keyid-format LONG | grep uid | sed 's#.*<##g;s#>.*##g')
+        uid='-r '$uid
+    fi
 
     [ "$account_name" == "" ] && \
         { echo "Usage: $0 service"; return 1; }
@@ -12,7 +50,7 @@ function ggp-encrypt-account-key
     [ -f "$key_file_encrypted" ] && \
         { echo "$0 - Error: Encrypted file "$key_file_encrypted" exists."; return 3; }
 
-    gpg2 --encrypt "$key_file"
+    gpg2 $uid --encrypt "$key_file"
 }
 
 function gpg-get-account-key
